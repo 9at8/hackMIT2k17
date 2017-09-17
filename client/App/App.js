2cebe20e60677a7791e93a83d5d89c36a23053b2
timotius02 <timsitorus1@gmail.com>
2017-09-16 20:10:28 -0400

    add client side code

--- client/App/App.js
+++ client/App/App.js
@@ -1,19 +1,114 @@
-import React from 'react'
-import { Switch, Route } from 'react-router-dom'
+import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
+import Masonry from "react-masonry-infinite";
+import shortid from "shortid";
+const colors = [
+  "#EC407A",
+  "#EF5350",
+  "#AB47BC",
+  "#7E57C2",
+  "#5C6BC0",
+  "#42A5F5",
+  "#29B6F6",
+  "#26C6DA",
+  "#26A69A",
+  "#66BB6A",
+  "#9CCC65",
+  "#827717",
+  "#EF6C00"
+];
 
-const App = (props) => {
-  return (
-    <div className="wrapper">
-      <h1>Hello World!</h1>
-      {/*<Navbar/>*/}
-      {/*<div className="main-container">*/}
-        {/*<Switch>*/}
-          {/*<Route exact path="/" component={}/>*/}
-          {/*<Route path="/search" component={}/>*/}
-        {/*</Switch>*/}
-      {/*</div>*/}
-    </div>
-  )
+const heights = [350, 350, 400, 400, 450, 450];
+
+const getRandomElement = array =>
+  array[Math.floor(Math.random() * array.length)];
+
+const generateElements = () =>
+  [...Array(10).keys()].map(() => ({
+    key: shortid.generate(),
+    color: getRandomElement(colors),
+    height: `${getRandomElement(heights)}px`
+  }));
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasMore: true,
+      elements: generateElements()
+    };
+  }
+
+  loadMore = () =>
+    setTimeout(
+      () =>
+        this.setState(state => ({
+          elements: state.elements.concat(generateElements())
+        })),
+      2500
+    );
+
+  componentDidMount() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        console.log(position.coords.latitude);
+        console.log(position.coords.longitude);
+
+        console.log(position.coords);
+        const fetchData = {
+          method: "POST",
+          mode: "cors",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+          })
+        };
+        fetch("/api/points-of-interest", fetchData).then(res => {
+          console.log(res.json());
+        });
+      });
+    } else {
+      console.log("Geolocation is not supported by this browser.");
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Destination Unknown? Try our recommendations</h1>
+        <div className="container">
+          <Masonry
+            className="masonry"
+            hasMore={this.state.hasMore}
+            loader={
+              <div className="sk-folding-cube">
+                <div className="sk-cube1 sk-cube" />
+                <div className="sk-cube2 sk-cube" />
+                <div className="sk-cube4 sk-cube" />
+                <div className="sk-cube3 sk-cube" />
+              </div>
+            }
+            loadMore={this.loadMore}
+          >
+            {this.state.elements.map(({ key, color, height }, i) => (
+              <div
+                key={key}
+                className="card"
+                style={{ background: color, height }}
+              >
+                <div className="content">
+                  <h2>Test</h2>
+                  <p>This is a fancy location.</p>
+                </div>
+              </div>
+            ))}
+          </Masonry>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,26 +1,30 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-let webpack = require('webpack');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+let webpack = require("webpack");
 
 let config = {
-  entry: './index.js',
+  entry: "./index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index_bundle.js',
-    publicPath: '/'
+    path: path.resolve(__dirname, "dist"),
+    filename: "index_bundle.js",
+    publicPath: "/"
   },
   module: {
     loaders: [
-      {test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/},
-      {test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/},
-      {test: /\.css$/, loader: ['style-loader', 'css-loader'], exclude: /node_modules/},
+      { test: /\.js$/, loader: "babel-loader", exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: "babel-loader", exclude: /node_modules/ },
+      {
+        test: /\.css$/,
+        loader: ["style-loader", "css-loader"],
+        exclude: /node_modules/
+      },
       {
         test: /\.(woff2?|ttf|svg|eot)(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'file-loader'
+        loader: "file-loader"
       },
       {
         test: /\.(png|jpg|gif)$/,
-        loader: 'url-loader',
+        loader: "url-loader",
         options: {
           limit: 8192
         }
@@ -30,27 +34,27 @@ let config = {
   devServer: {
     historyApiFallback: true,
     proxy: {
-      '/api': 'http://localhost:58080',
-      '/static': 'http://localhost:58080'
+      "/api": "http://localhost:3000",
+      "/static": "http://localhost:3000"
     }
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './index.html'
+      template: "./index.html"
     })
   ]
 };
 
-if (process.env.NODE_ENV === 'production') {
-  config.output.publicPath = '/static/'
+if (process.env.NODE_ENV === "production") {
+  config.output.publicPath = "/static/";
   config.plugins.push(
     new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      "process.env": {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV)
       }
     }),
     new webpack.optimize.UglifyJsPlugin()
-  )
+  );
 }
 
 module.exports = config;
